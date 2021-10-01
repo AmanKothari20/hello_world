9bf5464d117a40c6bef39dd535baab2b90088f47
NIKHIL MALKHEDE <56163387+Nkhil8625@users.noreply.github.com>
2021-10-01 14:33:50 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
-
+//sajbkjabkd
 #include <stdio.h>
 
 struct stacks{
